82fbb230517e7d38989acab3cbcda32b72dba654
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-09-23 10:48:29 +0800

    完成单链表结构

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
-using namespace std;
+struct Node;
+typedef struct Node *pNode;
 
+typedef pNode List;
+
+struct Node
+{
+	int value;
+	pNode next;
+};
+
+List makeEmpty ();
+
+bool isEmpty (List L);
+int getSize (List L);
+
+void insert_head (int Value, List L);
+void insert_tail (int Value, List L);
+void insert (int Value, int index, List L);
+
+void delete_head (List L);
+void delete_tail (List L);
+void deleteNode(int index, List L);
+
+int search (int Value, List L);
+int getHead (List L);
+int getTail (List L);
+
+List makeEmpty ()
+{
+	List l = (pNode)malloc (sizeof (struct Node));
+	l->next = NULL;
+	l->value = 0;
+	return l;
+}
+
+bool isEmpty (List L)
+{
+	return L->next == NULL;
+}
 
 int main ()
 {
