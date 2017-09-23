b79e76fadf7db0e9a88c7e8239b66a43763c6779
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-09-23 20:12:52 +0800

    完成linkedList注释

--- LinkedList/LinkedList/linkedList.cpp
+++ LinkedList/LinkedList/linkedList.cpp
@@ -1,24 +1,56 @@
+/*!
+ * \file linkedList.cpp
+ *
+ * \author fuy
+ * \date 九月 2017
+ *
+ * \brief linkedList.cpp单链表的结构和操作方法的实现
+ */
+
+
 #include "linkedList.h"
 
+
+//************************************
+// Method:    makeEmpty
+// Returns:   pNode
+// Parameter: int value
+// Description:创建并初始化一个节点，默认值为0，指针指向NULL
+//************************************
 pNode makeEmpty (int value)
 {
-    pNode p = (pNode) malloc (sizeof (struct Node));
+    pNode p = (pNode) malloc (sizeof (struct Node));//申请节点内存
+    //初始化节点元素和指针
     p->next = NULL;
     p->value = value;
     return p;
 }
 
+
+//************************************
+// Method:    isEmpty
+// Returns:   bool
+// Parameter: List L
+// Description: 判断链表L是否为空
+//************************************
 bool isEmpty (List L)
 {
-    return L->next == NULL;
+    return L->next == NULL;//链表第一个节点为头结点，元素在头结点后
 }
 
+
+//************************************
+// Method:    getSize
+// Returns:   int
+// Parameter: List L
+// Description: 获取链表中元素的个数（不包含头结点）
+//************************************
 int getSize (List L)
 {
     int size = 0;
     List l = L;
-    l = l->next;
-    while(l != NULL)
+    l = l->next;      //将指针从头结点移动到第一个元素
+    while(l != NULL)  //遍历链表，记录节点个数
     {
         size++;
         l = l->next;
@@ -26,122 +58,217 @@ int getSize (List L)
     return size;
 }
 
+
+//************************************
+// Method:    insert_head
+// Returns:   void
+// Parameter: int Value
+// Parameter: List L
+// Description: 在链表头部插入节点
+//************************************
 void insert_head (int Value, List L)
 {
-    pNode temp = makeEmpty (Value);
-    temp->next = L->next;
-    L->next = temp;
+    pNode temp = makeEmpty (Value); //创建并初始化插入的节点
+    temp->next = L->next;  //新节点指针指向第二个元素（可能为空，但不影响）
+    L->next = temp;        //头部指针指向新节点
 }
 
+
+//************************************
+// Method:    insert_tail
+// Parameter: int Value
+// Parameter: List L
+// Description: 在链表尾部插入节点
+//************************************
 void insert_tail (int Value, List L)
 {
     pNode temp = makeEmpty (Value);
-    List l = L;
+    List l = L;            //新建指针，用于遍历链表
 
-    while(l->next != NULL)
+    while(l->next != NULL) //遍历链表，直到指针节点为尾部元素（指针指向NULL）
     {
         l = l->next;
     }
-    l->next = temp;
+    l->next = temp;        //完成插入操作
 }
 
+
+//************************************
+// Method:    insert
+// Parameter: int Value
+// Parameter: int index
+// Parameter: List L
+// Return:    bool
+// Description: 在index处插入节点，插入后该节点的索引值为index
+//************************************
 bool insert (int Value, int index, List L)
 {
+    //判断索引值是否合法（上界:size；下界:0）
+    //如果越界，打印错误提示信息，返回false，否则插入新节点并返回true
     if(getSize (L) < index || index < 0)
     {
         logError ("Insert Error: Insert index is lower than zero or larger than size of ist!!!\r\n");
         return false;
     }
 
-    List l = L;
-    pNode temp = makeEmpty (Value);
+    List l = L;//创建指针，用于遍历链表
+    pNode temp = makeEmpty (Value);//创建并初始化新节点
 
+    //将指针移动到index节点的前一个节点
     for(int i = 0; i < index; i++)
     {
         l = l->next;
     }
 
+    //变换节点指针，完成插入操作
     temp->next = l->next;
     l->next = temp;
+
     return true;
 }
 
+
+//************************************
+// Method:    delete_head
+// Parameter: List L
+// Description: 删除第一个节点
+//************************************
 void delete_head (List L)
 {
-    if(L->next == NULL)
+    if(L->next == NULL)//如果链表为空，则没有元素可删除，直接返回
         return;
 
+    //变换指针指向，将第一个节点隔离开来
     pNode temp = L->next;
     L->next = L->next->next;
-    free (temp);
+
+    free (temp);//删除第一个元素节点
 }
 
+
+//************************************
+// Method:    delete_tail
+// Parameter: List L
+// Description: 删除尾部节点（也可以用deleteNode(size-1,L)完成）
+//************************************
 void delete_tail (List L)
 {
-    if(L->next == NULL)
+    if(L->next == NULL)//如果链表为空，则没有元素可删除，直接返回
         return;
 
+    //获取链表元素个数和创建遍历指针
     int size = getSize (L);
     List l = L;
 
+    //将遍历指针移动到尾部元素的前一个节点
     for(int i = 1; i < size; i++)
     {
         l = l->next;
     }
 
+    //尾部元素是链表最后一个元素，可直接free，然后将前一元素指针指向NULL
     free (l->next);
     l->next = NULL;
 }
 
+
+//************************************
+// Method:    deleteNode
+// Parameter: int index
+// Parameter: List L
+// Description: 删除索引为index的节点
+//************************************
 void deleteNode (int index, List L)
 {
-    if(isEmpty (L))
+    if(isEmpty (L))//如果链表为空，则没有元素可删除，直接返回
         return;
+
+    //判断索引值是否合法（上界:size-1；下界:0）
+    //如果索引值越界，打印错误提示信息，直接返回
     if(index > getSize (L) - 1 || index < 0)
     {
         logError ("Insert Error: Insert index is lower than zero or larger than size of ist!!!\r\n");
         return;
     }
 
+    //将指针指向index节点的前一个节点
     for(int i = 0; i < index; i++)
     {
         L = L->next;
     }
 
+    //完成指针变换，free节点
     pNode temp = L->next->next;
     free (L->next);
     L->next = temp;
 }
 
 
+//************************************
+// Method:    deleteList
+// Parameter: List L
+// Returns:   void
+// Description: 删除链表中所有元素(包括头结点)，清空后链表指针失效
+//************************************
+void deleteList (List L)
+{
+    List l = L;
+    while(L != NULL)
+    {
+        l = L;
+        L = L->next;
+        free (l);
+    }
+}
+
+
+//************************************
+// Method:    getNode
+// Parameter: int index
+// Parameter: List L
+// Returns:   pNode
+// Description: 获取index处的节点指针
+//************************************
 pNode getNode (int index, List L)
 {
-    if(isEmpty (L))
+    if(isEmpty (L))//如果链表为空，则没有元素可获取，直接返回NULL
         return NULL;
 
+    //判断索引值是否合法（上界:size-1；下界:0）
+    //如果索引值越界，打印错误提示信息，直接返回
     if(index > getSize (L) - 1 || index < 0)
     {
         logError ("Index Error: Index is lower than zero or larger than (size-1) of ist!!!\r\n");
         return NULL;
     }
-    pNode pN = L;
 
+    //创建遍历指针，并将指针移动到index处
+    pNode pN = L;
     for(int i = -1; i < index; i++)
     {
         pN = pN->next;
     }
+
     return pN;
 }
 
+//************************************
+// Method:    search
+// Parameter: int Value
+// Parameter: List L
+// Returns:   int
+// Description: 搜索链表是否有某一元素，如果链表有多个值为value的元素，则只返回第一个的索引
+//              如果没有该元素，返回NotInList
+//************************************
 int search (int Value, List L)
 {
+    //创建索引值和遍历指针
     int index = 0;
-
     L = L->next;
 
-    while(L != NULL)
+    while(L != NULL)          //遍历链表
     {
-        if(L->value == Value)
+        if(L->value == Value) //逐一对比元素，若节点值与搜索值相同，直接返回索引
         {
             return index;
         }
@@ -152,41 +279,74 @@ int search (int Value, List L)
     return NotInList;
 }
 
+
+//************************************
+// Method:    getValve
+// Parameter: int index
+// Parameter: List L
+// Returns:   int
+// Description: 获取链表中索引为index处的元素值
+//************************************
 int getValve (int index, List L)
 {
-    if(isEmpty (L))
+    if(isEmpty (L))//如果链表为空，则没有元素可获取，直接返回EmptyList
         return EmptyList;
+
+    //判断索引值是否合法（上界:size-1；下界:0）
+    //如果索引值越界，打印错误提示信息，直接返回OutOfRange
     if(index > getSize (L) - 1 || index < 0)
     {
         logError ("Index Error: Index is lower than zero or larger than (size-1) of ist!!!\r\n");
         return OutOfRange;
     }
 
+    //将指针移动到目标索引处
     L = L->next;
     for(int i = 0; i < index; i++)
     {
         L = L->next;
     }
+
     return L->value;
 }
 
 
+//************************************
+// Method:    getHead
+// Parameter: List L
+// Returns:   int
+// Description: 获取链表第一个元素
+//************************************
 int getHead (List L)
 {
-    if(isEmpty (L))
-        return false;
+    if(isEmpty (L))//如果链表为空，则没有元素可获取，直接返回EmptyList
+        return EmptyList;
     return L->next->value;
 }
 
+
+//************************************
+// Method:    getTail
+// Parameter: List L
+// Returns:   int
+// Description: 获取链表尾部元素
+//************************************
 int getTail (List L)
 {
+    //直接调用getValve()函数获取
     return getValve (getSize (L) - 1, L);
 }
 
+//************************************
+// Method:    traversal_List
+// Parameter: List L
+// Returns:   void
+// Description:
+//************************************
 void traversal_List (List L)
 {
-    List l = L->next;
-    while(l != NULL)
+    List l = L->next;  //移动指针到第一个元素
+    while(l != NULL)   //开始遍历，逐一打印
     {
         printf ("%d\t", l->value);
         l = l->next;
--- LinkedList/LinkedList/linkedList.h
+++ LinkedList/LinkedList/linkedList.h
@@ -1,42 +1,89 @@
+/*!
+ * \file linkedList.h
+ *
+ * \author fuy
+ * \date 九月 2017
+ *
+ * \brief linkedList.h声明了单链表的结构和操作方法
+ */
+
+/************************************************************************/
+/*
+/*  linkedList.h和linkedList.cpp文件实现的链表为单向链表，有头结点
+/*  
+/*　支持的操作：
+/*      两端取值、插值以及删值；
+/*      搜索节点和获取某一索引处的值；
+/*      遍历链表和删除链表操作
+/*      判断链表是否为空和获取链表元素个数
+/*      返回索引处的节点指针
+/*    
+/*  链表结构：
+/*      index:    NULL      0         1         2               n-1
+/*      节点:     head -> index1 -> index2 -> index3 -> ... -> indexn -> NULL        
+/*      value:    NULL    value1    value2    value3           valuen   
+/*
+/*  头结点（head）的index和value都为空，无实际意义
+/*  头结点的存在是为了方便遍历链表（标记节点），上述操作如
+/*      计算链表元素个数并不会将head节点计入；
+/*      删除、插入或者查询head等操作实际也是指链表index=0的元素，并不是头结点
+/*      遍历链表时也不会将head的value打印出来
+/*
+/*  头结点也可以用头指针代替（与头结点相比，只有next指针，没有元素）
+/*  
+/*  ***************** 链表中头结点，操作时容易引起错误 *********************
+/*
+/************************************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 
+
+// 链表只有logError(x) 和遍历链表函数用到了printf()函数，用于输出提示信息
 #define logError(x) printf(x)
 
+// 在查询或者插入节点等操作时，index可能越界，链表也可能为空，返回下面的数即表示越界或链表为空
 #define NotInList -65534
 #define OutOfRange -65533
-#define EmptyList -65533
+#define EmptyList -65532
 
+// 链表结构体声明和结构体指针类型定义
 struct Node;
 typedef struct Node *pNode;
-
 typedef pNode List;
 
+// 链表结构体定义
+// value：节点数值
+// next：节点指针，指向下一个节点
 struct Node
 {
     int value;
     pNode next;
 };
 
-pNode makeEmpty (int value = 0);
-bool isEmpty (List L);
-int getSize (List L);
 
-void insert_head (int Value, List L);
-void insert_tail (int Value, List L);
-bool insert (int Value, int index, List L);
+pNode makeEmpty (int value = 0); //创建新节点，用value初始化
+bool isEmpty (List L);           //判断链表是否为空   
 
-void delete_head (List L);
-void delete_tail (List L);
-void deleteNode (int index, List L);
+int getSize (List L);            //获取链表元素大小
 
-int search (int Value, List L);
+void insert_head (int Value, List L);       //在链表头部插入元素
+void insert_tail (int Value, List L);       //在链表尾部插入元素
+bool insert (int Value, int index, List L); //在index处插入元素
 
-pNode getNode (int index, List L);
+void delete_head (List L);           //删除头部元素
+void delete_tail (List L);           //删除尾部元素
+void deleteNode (int index, List L); //删除index处元素
+void deleteList (List L);            //清空链表(包括头部节点)
 
-int getHead (List L);
-int getTail (List L);
-int getValve (int index, List L);
+int search (int Value, List L);      //搜索链表中值为value的元素，返回其index，没有则返回NotInList（如果有多个值为value的元素，则只返回第一个元素的索引值）
 
-void traversal_List (List L);
+pNode getNode (int index, List L);   //返回链表中index处的节点指针
+
+int getHead (List L);                //返回头部元素
+int getTail (List L);                //返回尾部元素
+int getValve (int index, List L);    //返回index处的元素
+
+void traversal_List (List L);        //遍历链表
