8355c74ea4c54efb8386de8532b7b8cc737273a6
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-09-24 16:32:41 +0800

    创建数组栈结构

---
+++ LinkedList/LinkedList/Stack/ArrayStack/arrayStack.h
@@ -0,0 +1,7 @@
+
+#ifndef __ARRAYSTACK_H__
+#define __ARRAYSTACK_H__
+
+
+
+#endif
--- LinkedList/LinkedList/Stack/main.cpp
+++ LinkedList/LinkedList/Stack/main.cpp
@@ -1,22 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "stack/stack.h"
+#include <malloc.h>
+//#include "stack/stack.h"
+#include "arrayStack/arrayStack.h"
+
+#define logError(x) printf(x)
+
+struct StackRecord;
+typedef StackRecord *arrayStack;
+
+struct StackRecord
+{
+    int capacity;
+    int topOfStack;
+    int *array;
+};
+
+bool isEmpty (arrayStack S);
+bool isFull (arrayStack S);
+
+arrayStack createStack (int cpacity);
+
+void emptyStack (arrayStack S);
+void clearStack (arrayStack S);
+
+bool push (arrayStack S);
+int pop (arrayStack S);
+
+int top (arrayStack S);
+
+
+bool isEmpty (arrayStack S)
+{
+    return S->topOfStack == -1;
+}
+bool isFull (arrayStack S)
+{
+    return S->topOfStack == S->capacity - 1;
+}
+
+arrayStack createStack (int capacity)
+{
+    arrayStack S = (StackRecord*) malloc (sizeof (struct StackRecord));
+    
+    if(S == NULL)
+        logError ("Out of space!!!");
+
+    S->array = (int *)malloc (sizeof (int) * capacity);
+    S->capacity = capacity;
+
+    if(S->capacity == NULL)
+        logError ("Out of space!!!");
+
+    S->topOfStack = -1;
+
+    return S;
+}
+
+void emptyStack (arrayStack S);
+void clearStack (arrayStack S);
+
+bool push (arrayStack S);
+int pop (arrayStack S);
+
+int top (arrayStack S);
+
 
 int main ()
 {
-    Stack s = makeEmpty ();
-    push (1, s);
-    push (2, s);
-    push (3, s);
-    top (s);
-    pop (s);
-    pop (s);
-    push (4, s);
-    push (6, s);
-    top (s);
-    top (s);
-    printf("%d", getSize (s));
-
     return 0;
 }
 
