0b4261fd4d343a98ad370b9bf7bec01dfeef21b2
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-10-07 11:29:31 +0800

    完成数组式队列的注释

--- LinkedList/LinkedList/Queue/ArrayQueue/arrayQueue.cpp
+++ LinkedList/LinkedList/Queue/ArrayQueue/arrayQueue.cpp
@@ -1,33 +1,75 @@
+/*!
+ * \file arrayQueue.cpp
+ * \date 2017/10/07 11:08
+ *
+ * \author fuy
+ *
+ * \brief arrayQueue.cpp 实现数组式队列
+ * 
+ */
+
+
 #include "arrayQueue.h"
 
+
+//************************************
+// Method:    isEmpty
+// Parameter: arrayQueue Q
+// Returns:   bool
+// Description: 判断队列是否为空
+//************************************
 bool isEmpty (arrayQueue Q)
 {
     return Q->size == 0;
 }
 
+
+//************************************
+// Method:    isFull
+// Parameter: arrayQueue Q
+// Returns:   bool
+// Description: 判断队列是否已满
+//************************************
 bool isFull (arrayQueue Q)
 {
     return Q->size == Q->capacity;
 }
 
 
+//************************************
+// Method:    getSize
+// Parameter: arrayQueue Q
+// Returns:   int
+// Description: 获取队列元素数目
+//************************************
 int getSize (arrayQueue Q)
 {
     return Q->size;
 }
 
+//************************************
+// Method:    createArrayQueue
+// Parameter: int capacity
+// Returns:   arrayQueue
+// Description: 创建一个新的队列
+//************************************
 arrayQueue createArrayQueue (int capacity)
 {
+    // 申请队列结构体内存
     arrayQueue Q = (pQueue) malloc (sizeof (struct QueueRecord));
 
+    // 判断是否申请成功
     if(Q == NULL)
         logError ("Out of space!!!");
 
+    // 申请队列数组内存
     Q->array = (int *) malloc (sizeof (int) * capacity);
 
+    // 判断是否申请成功
     if(Q->array == NULL)
         logError ("Out of space!!!");
 
+    // 初始化队列参数（数目，头尾索引）
     Q->size = 0;
     Q->front = 0;
     Q->rear = 0;
@@ -36,10 +78,20 @@ arrayQueue createArrayQueue (int capacity)
 }
 
 
+//************************************
+// Method:    enqueue
+// Parameter: int value 新元素数值
+// Parameter: arrayQueue Q
+// Returns:   bool 
+// Description: 将新元素插入队列
+//************************************
 bool enqueue (int value, arrayQueue Q)
 {
+    // 如果队列已满，插入失败，返回false
     if(isFull (Q))
         return false;
+
+    // 插入元素；变换rear索引；修改队列size参数
     Q->array[Q->rear] = value;
     Q->rear = (Q->rear + 1) % Q->capacity;
     Q->size++;
@@ -47,11 +99,20 @@ bool enqueue (int value, arrayQueue Q)
     return true;
 }
 
+
+//************************************
+// Method:    dequeue
+// Parameter: arrayQueue Q
+// Returns:   int
+// Description: 从队列中取出一个元素
+//************************************
 int dequeue (arrayQueue Q)
 {
+    // 如果队列为空，取出失败，返回空队列标志
     if(isEmpty (Q))
         return EmptyQueue;
-
+    
+    // 取出元素；完成索引变换；修改队列参数
     int res = Q->array[Q->front];
     Q->front = (Q->front + 1) % Q->capacity;
     Q->size--;
@@ -59,19 +120,44 @@ int dequeue (arrayQueue Q)
     return res;
 }
 
+
+//************************************
+// Method:    front
+// Parameter: arrayQueue Q
+// Returns:   int
+// Description: 获取队列头部元素
+//************************************
 int front (arrayQueue Q)
 {
+    // 如果队列为空，返回空队列标志
     if(isEmpty (Q))
         return EmptyQueue;
+
     return Q->array[Q->front];
 }
 
+
+//************************************
+// Method:    clearQueue
+// Parameter: arrayQueue Q
+// Returns:   void
+// Description: 清空队列中的元素
+//************************************
 void clearQueue (arrayQueue Q)
 {
+    // 清空操作实质是改变队列参数
     Q->size = 0;
     Q->front = 0;
     Q->rear = 0;
 }
+
+
+//************************************
+// Method:    deleteQueue
+// Parameter: arrayQueue Q
+// Returns:   void
+// Description: 删除队列，释放内存
+//************************************
 void deleteQueue (arrayQueue Q)
 {
     if(Q != NULL)
@@ -81,12 +167,20 @@ void deleteQueue (arrayQueue Q)
     }
 }
 
+//************************************
+// Method:    traversal_Queue
+// Parameter: arrayQueue Q
+// Returns:   void
+// Description: 遍历队列
+//************************************
 void traversal_Queue (arrayQueue Q)
 {
-    int index = Q->front;
+    int index = Q->front; //从队列头部开始索引
+
     for(int i = 0; i < getSize (Q); i++)
     {
         printf ("%d\t", Q->array[(Q->front + i) % Q->capacity]);
     }
     printf ("\r\n");
 }
+
--- LinkedList/LinkedList/Queue/ArrayQueue/arrayQueue.h
+++ LinkedList/LinkedList/Queue/ArrayQueue/arrayQueue.h
@@ -1,42 +1,77 @@
+/*!
+ * \file arrayQueue.h
+ * \date 2017/10/07 10:51
+ *
+ * \author fuy
+ *
+ * \brief arrayQueue.h和arrayQueue.cpp声明和实现了数组式队列
+ *
+ */
+
+ /************************************************************************/
+ /*
+ /*  arrayQueue.h和arrayQueue.cpp文件实现了数组式队列结构
+ /*
+ /*　支持的操作：
+ /*      enqueue,dequeue,front（在栈顶进行）
+ /*      遍历、清空和删除队列
+ /*      判断队列是否为空和获取队列元素个数
+ /*
+ /*  数组结构：
+ /*                                     front             rear
+ /*      index:       0         1         2      ...       n-1
+ /*      value:     value1    value2    value3   ...      valuen
+ /*
+ /*  队列的从rear端插入新的元素，从front端取出元素，所以在操作中front和rear的
+ /*  索引值会发生变化，没有固定的位置
+ /*  
+ /*
+ /************************************************************************/
+
+
 #ifndef __ARRAYQUEUE_H__
 #define __ARRAYQUEUE_H__
 
 #include <stdio.h>
 #include <stdlib.h>
 
+// 输出错误提示信息
 #define logError(x) printf(x)
 
-//空队列标志，当尝试从空队列中获取或删除节点时，返回该标志
+// 空队列标志，当尝试从空队列中获取或删除节点时，返回该标志
 #define EmptyQueue -65534
 
+// 声明数组式队列的数据结构及其指针类型
 struct QueueRecord;
 typedef QueueRecord *pQueue;
 typedef pQueue arrayQueue;
 
+
+// 定义数组式队列的结构体
 struct QueueRecord
 {
-    int capacity;
-    int front;
-    int rear;
-    int size;
-    int *array;
+    int capacity; // 队列的大小
+    int front;    // 队列头部（从此处取出元素）
+    int rear;     // 队列尾部（从此处添加新元素）
+    int size;     // 队列元素打小
+    int *array;   // 存放元素的数组（使用前需要初始化）
 };
 
-bool isEmpty (arrayQueue Q);
-bool isFull (arrayQueue Q);
+bool isEmpty (arrayQueue Q); // 判断队列是否为空
+bool isFull (arrayQueue Q);  // 判断队列是否已满
 
-int getSize (arrayQueue Q);
+int getSize (arrayQueue Q);  // 获取队列元素数目
 
-arrayQueue createArrayQueue (int cpacity);
+arrayQueue createArrayQueue (int cpacity); // 创建并初始化一个队列
 
-bool enqueue (int value, arrayQueue Q);
-int dequeue (arrayQueue Q);
+bool enqueue (int value, arrayQueue Q);    // 添加一个新元素进入队列
+int dequeue (arrayQueue Q);                // 从队列中出去一个新元素
 
-int front (arrayQueue Q);
+int front (arrayQueue Q);                  // 查看队列头部元素
 
-void clearQueue (arrayQueue Q);
-void deleteQueue (arrayQueue Q);
+void clearQueue (arrayQueue Q);       // 清空队列元素（仅仅改变索引值）
+void deleteQueue (arrayQueue Q);      // 删除队列，释放内存
 
-void traversal_Queue (arrayQueue Q);
+void traversal_Queue (arrayQueue Q);  // 遍历队列
 
 #endif
