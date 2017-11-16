b8f1cdaa890fd1e35fabf276edb2378719ac6261
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-11-16 12:22:28 +0800

    add some code, dlist :insert delete get node

--- LinkedList/LinkedList/DoublyLinkedList/main.cpp
+++ LinkedList/LinkedList/DoublyLinkedList/main.cpp
@@ -19,7 +19,7 @@ struct Node
 struct doublyLinkedList
 {
     pNode head;
-    pNode rear;
+    pNode tail;
 };
 
 
@@ -72,16 +72,16 @@ DList makeEmptyDList ()
         logError ("Out of Space!!!");
 
     d->head = makeEmptyNode ();
-    d->rear = makeEmptyNode ();
-    d->head->next = d->rear;
-    d->rear->piror = d->head;
+    d->tail = makeEmptyNode ();
+    d->head->next = d->tail;
+    d->tail->piror = d->head;
     return d;
 }
 
 
 bool isEmpty (DList L)
 {
-    return L->head->next == L->rear;
+    return L->head->next == L->tail;
 }
 
 int getSize (DList L)
@@ -89,7 +89,7 @@ int getSize (DList L)
     pNode p = L->head;
     int size = 0;
 
-    while(p->next != L->rear)
+    while(p->next != L->tail)
     {
         size++;
         p = p->next;
@@ -113,24 +113,92 @@ void insert_tail (int Value, DList L)
 {
     pNode p = makeEmptyNode (Value);
 
-    p->piror = L->rear->piror;
-    L->rear->piror->next = p;
+    p->piror = L->tail->piror;
+    L->tail->piror->next = p;
 
 
-    L->rear->piror = p;
-    p->next = L->rear;
+    L->tail->piror = p;
+    p->next = L->tail;
 }
 
-bool insert(int Value, int index, DList L);
+bool insert (int Value, int index, DList L)
+{
 
-void delete_head(DList L);
-void delete_tail(DList L);
-void deleteNode(int index, DList L);
+    pNode p = getNode (index,L);
+    if(p == NULL)
+        return false;
+
+    pNode temp = makeEmptyNode (Value);
+
+    pNode pre = p->piror;
+
+    pre->next = temp;
+    temp->piror = pre;
+
+    temp->next = p;
+    p->piror = temp;
+
+    return true;
+}
+
+void delete_head (DList L)
+{
+    if(isEmpty (L))
+        return;
+    pNode p = L->head->next;
+
+    L->head->next = L->head->next->next;
+    L->head->next->piror = L->head;
+
+    free (p);
+}
+
+void delete_tail (DList L)
+{
+    if(isEmpty (L))
+        return;
+    pNode p = L->tail->piror;
+
+    L->tail->piror = L->tail->piror->piror;
+    L->tail->piror->next = L->tail;
+
+    free (p);
+}
+void deleteNode (int index, DList L)
+{
+    pNode p = getNode (index, L);
+    if(p == NULL)
+        return;
+
+    pNode temp = p->piror;
+
+    temp->next = p->next;
+    p->next->piror = temp;
+
+    free (p);
+}
 void deleteList(DList L);
 
 int search(int Value, DList L);
 
-pNode getNode(int index, DList L);
+pNode getNode (int index, DList L)
+{
+    int size = getSize (L);
+    if(index < 0 | index > size)
+    {
+        logError ("index out of range!!!");
+        return NULL;
+    }
+
+    pNode p = L->head->next;
+    int i = 0;
+    while(i != index)
+    {
+        i++;
+        p = p->next;
+    }
+    return p;
+}
 
 int getHead(DList L);
 int getTail(DList L);
@@ -146,6 +214,16 @@ int main ()
 
     insert_head (2, d);
     insert_tail (3, d);
+    insert_head (4, d);
+    insert_tail (5, d);
+
+    insert (8, 4, d);
+    insert (7, 2, d);
+    insert (6, 3, d);
+
+
+    delete_head (d);
+    delete_tail (d);
 
     isEmpty (d);
     getSize (d);
