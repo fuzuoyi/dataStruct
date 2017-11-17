de2d2588d9542f97d9ffe981dd5bd3c6d47b3e4f
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-11-17 20:57:17 +0800

    test func time

--- Sorting/Sorting/main.cpp
+++ Sorting/Sorting/main.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include<string.h>
+#include <time.h>
+
 #include "Sort/sort.h"
 
+typedef void (*SortFunc)(int *,int);   
 
 
+clock_t time_test (SortFunc, int array[], int arrayLength, int count);
+
+void getRandArray (int arrayLength, int array[]);
    
 
 int main ()
@@ -15,23 +22,64 @@ int main ()
 
     int size = sizeof (data) / sizeof (int);
 
-    for (int i = 0;i<size;i++)
-    {
-        printf ("%d\t",data[i]);
-    }
-    printf ("\r\n");
+    //for (int i = 0;i<size;i++)
+    //{
+    //    printf ("%d\t",data[i]);
+    //}
+    //printf ("\r\n");
 
 
-    mergeSort (data, size);
+    //mergeSort (data, size);
 
+    //for(int i = 0; i < size; i++)
+    //{
+    //    printf ("%d\t",data[i]);
+    //}
+
+    //printf ("\r\n");
+
+    SortFunc sort = &insertionSort;
+
+    clock_t t = time_test (sort, data,size,10000);
     for(int i = 0; i < size; i++)
     {
-        printf ("%d\t",data[i]);
+        printf ("%d\t", data[i]);
     }
-
-    printf ("\r\n");
+    printf ("time: %ld\r\n", t);
 
     system ("pause");
     return 0;
 }
 
+void getRandArray (int arrayLength, int array[])
+{
+
+}
+
+
+clock_t time_test (SortFunc sort, int num_array[], int arrayLength, int count)
+{
+    clock_t start, end, duration=0;
+
+    int *temp_array = (int*) malloc (arrayLength * sizeof (int));
+    memcpy (temp_array, num_array, arrayLength * sizeof (int));
+
+
+    
+
+    for(int i = 0; i < count; i++)
+    {
+        
+        memcpy (num_array, temp_array, arrayLength * sizeof (int));
+
+        start = clock ();
+        sort (num_array, arrayLength);
+        end = clock ();
+
+        duration += end - start;
+    }
+
+    free (temp_array);
+
+    return duration;
+}
