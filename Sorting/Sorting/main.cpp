6eed92098d0636e787bce399f05af49a5370a1b3
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-11-17 22:02:16 +0800

    add utils file

--- Sorting/Sorting/main.cpp
+++ Sorting/Sorting/main.cpp
@@ -5,14 +5,8 @@
 #include <time.h>
 
 #include "Sort/sort.h"
+#include "utils/utils.h"
 
-typedef void (*SortFunc)(int *,int);   
-
-
-clock_t time_test (SortFunc, int array[], int arrayLength, int count=1);
-void PrintIntArray (int num_array[], int arrayLength);
-
-void getIntRandArray (int num_array[], int arrayLength);
    
 
 int main ()
@@ -43,61 +37,3 @@ int main ()
 }
 
 
-void PrintIntArray (int num_array[], int arrayLength)
-{
-    for (int i = 0;i<arrayLength;i++)
-    {
-        printf ("%d\t", num_array[i]);
-    }
-    printf ("\r\n");
-}
-
-void getIntRandArray (int num_array[],int arrayLength)
-{
-    int index = arrayLength - 1;
-    int rand_value;
-
-    int *usedIndex = (int*) malloc (arrayLength * sizeof (int));
-   
-    for(int i = 0; i < arrayLength; i++)
-        usedIndex[i] = 0;
-
-    srand((unsigned)time(0));
-
-    while (index >= 0)
-    {
-        rand_value = rand() % arrayLength;
-    	if (usedIndex[rand_value] == 1) continue;
-    	else
-    	{
-    		usedIndex[rand_value] = 1;
-            num_array[index] = rand_value;
-            index--;
-    	}
-    }
-}
-
-
-clock_t time_test (SortFunc sort, int num_array[], int arrayLength, int count)
-{
-    clock_t start, end, duration=0;
-
-    int *temp_array = (int*) malloc (arrayLength * sizeof (int));
-    memcpy (temp_array, num_array, arrayLength * sizeof (int));
-
-    for(int i = 0; i < count; i++)
-    {
-        
-        memcpy (num_array, temp_array, arrayLength * sizeof (int));
-
-        start = clock ();
-        sort (num_array, arrayLength);
-        end = clock ();
-
-        duration += end - start;
-    }
-
-    free (temp_array);
-
-    return duration;
-}
---
+++ Sorting/Sorting/utils/utils.h
@@ -0,0 +1,21 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include<string.h>
+#include <time.h>
+
+typedef void (*SortFunc)(int *, int);
+
+
+clock_t time_test (SortFunc, int array[], int arrayLength, int count = 1);
+void PrintIntArray (int num_array[], int arrayLength);
+
+void getIntRandArray (int num_array[], int arrayLength);
+
+
+
+
+#endif // !__UTILS_H__
