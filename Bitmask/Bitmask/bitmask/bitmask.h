8f3f0c79d930206d16ff8b822c934f08c4dd87a9
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-11-20 16:27:33 +0800

    add comment

--- Bitmask/Bitmask/bitmask/bitmask.h
+++ Bitmask/Bitmask/bitmask/bitmask.h
@@ -7,11 +7,15 @@
 // 二进制格式输出
 void printf_bin (int S);
 
-int setBit (int S, int index);
-bool checkBit (int S, int index);
-int toggleBit (int S, int index);
-int clearBit (int S, int index);
-int leastSignificantBit (int S);
+
+bool checkBit (int S, int index); // 位状态
+
+
+int setBit (int S, int index);    // 位置高
+int toggleBit (int S, int index); // 位翻转
+int clearBit (int S, int index);  // 位清零
+
+int leastSignificantBit (int S);  // 最小有效位
 
 
 
