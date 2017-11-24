4f439cb2cf6741d42877d87cf3c047dff2f1fa8a
fuzuoyi <fuzuoyi@hust.edu.cn>
2017-11-24 20:35:35 +0800

    comment

--- Sorting/Sorting/Sort/quickSort.cpp
+++ Sorting/Sorting/Sort/quickSort.cpp
@@ -8,7 +8,7 @@ void quickSort (int array[], int arrayLength);
 int _median3 (int array[], int left, int right);
 void _QSort (int array[], int left, int right);
 
-
+// 三数中值分割，使得 “左 < 中 < 右”，然后将 “中” 和“后”置换
 int _median3 (int array[], int left, int right)
 {
     int center = (left + right) / 2;
@@ -21,6 +21,7 @@ int _median3 (int array[], int left, int right)
     if(array[center] > array[right])
         swap (array, center, right);
 
+    // 将中值置换到最后一位
     swap (array, center, right - 1);
 
     return array[right - 1];
@@ -33,22 +34,29 @@ void _QSort (int array[], int left, int right)
 
     if(left + Cutoff <= right)
     {
+        // 获取枢纽元
         pivot = _median3 (array, left, right);
 
+        // 索引值
         i = left;
         j = right - 1;
 
         for(;;)
         {
+            // 移动游标
             while(array[++i]<pivot) {}
             while(array[--j]>pivot) {}
 
+          
             if(i < j)
+                // 交换元素，将小于枢纽元的元素放在左边，大于枢纽元的元素放在右边
                 swap (array, i, j);
             else
+                // 两个子集划分完成后，退出循环
                 break;
         }
 
+        // 将枢纽元置换到合适的位置
         swap (array, i, right - 1);
 
         _QSort (array, left, i - 1);
