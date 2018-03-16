719f946621cb637a5e67d0760433497b2bc54887
fuzuoyi <fuzuoyi@hust.edu.cn>
2018-03-16 15:13:06 +0800

    erty

--- Tree/Tree/main.cpp
+++ Tree/Tree/main.cpp
@@ -36,9 +36,64 @@ int main()
 }
 
 
-SearchTree makeEmpty(SearchTree T);
-pTNode find(int x, SearchTree T);
-pTNode findMin(SearchTree T);
-pTNode findMax(SearchTree T);
-SearchTree insert(int x, SearchTree T);
+SearchTree makeEmpty (SearchTree T)
+{
+    if(T != NULL)
+    {
+        makeEmpty (T->left);
+        makeEmpty (T->right);
+        free (T);
+    }
+    return NULL;
+}
+
+pTNode find (int x, SearchTree T)
+{
+    if(T == NULL)
+        return NULL;
+    if(x < T->value)
+        return find (x, T->left);
+    else if(x > T->value)
+        return find (x, T->right);
+    else
+        return T;
+}
+
+
+pTNode findMin (SearchTree T)
+{
+    // 递归实现，findMax同样可以用递归来实现
+    if(T == NULL)
+        return NULL;
+    else
+    {
+        if(T->left == NULL)
+            return T;
+        else
+            return findMin (T->left);
+    }
+}
+
+
+pTNode findMax (SearchTree T)
+{
+    // 非递归实现，findMin同样可以用非递归来实现
+    if(T != NULL)
+    {
+        while(T->right != NULL)
+            T = T->right;
+        return T;
+    }
+}
+
+
+SearchTree insert (int x, SearchTree T)
+{
+
+}
+
+
 SearchTree deleteNode(int x, SearchTree T);
+
+
+
