601240aa0381953a2cc7bd4290acd25094cc9697
fuzuoyi <fuzuoyi@hust.edu.cn>
2018-03-23 19:36:34 +0800

    comment

--- Tree/Tree/AVLTree/avlTree/avlTree.h
+++ Tree/Tree/AVLTree/avlTree/avlTree.h
@@ -32,18 +32,20 @@ struct AVLNode
 
 pTNode makeEmpty (int x, int height = 0);
 
+// 获取树高
 int height (pTNode P);
 
 pTNode find (AVL T, int x);
 pTNode findMin (AVL T);
 pTNode findMax (AVL T);
 
+// 插入 / 删除
 void insert (AVL &T, int x);
 void deleteNode (AVL &T, int x);
 void deleteTree (AVL &T);
 
 
-
+// 旋转操作
 pTNode _singleRotateWithLeft (AVL root);  
 pTNode _singleRotateWithRight (AVL root);
 pTNode _doubleRotateWithLeft (AVL root);
